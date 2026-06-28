9010dbb18910944727afc01ed06831f60a2095d3
김진호 <111578154+jinhokingofworld@users.noreply.github.com>
2026-06-28 17:45:45 +0900

    feat: expose voice room API (#249)

--- apps/app-server/src/app.module.ts
+++ apps/app-server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { HealthController } from "./health.controller";
 import { MeetingModule } from "./modules/meeting/meeting.module";
+import { VoiceModule } from "./modules/voice/voice.module";
 
 @Module({
-  imports: [MeetingModule],
+  imports: [MeetingModule, VoiceModule],
   controllers: [HealthController],
 })
 export class AppModule {}
