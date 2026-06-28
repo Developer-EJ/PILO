fa38eb4d77c84ff1c41e8b9d690a552424bc0817
김진호 <111578154+jinhokingofworld@users.noreply.github.com>
2026-06-28 17:04:49 +0900

    feat: expose meeting CRUD status APIs (#242)

--- apps/app-server/src/modules/meeting/meeting.module.ts
+++ apps/app-server/src/modules/meeting/meeting.module.ts
@@ -1,4 +1,6 @@
 import { Module } from "@nestjs/common";
+import { CURRENT_MEMBER_ADAPTER } from "./adapters/current-member.adapter";
+import { MockCurrentMemberAdapter } from "./adapters/mock-current-member.adapter";
 import { MeetingController } from "./meeting.controller";
 import { MEETING_REPOSITORY } from "./repositories/meeting.repository";
 import { MockMeetingRepository } from "./repositories/meeting.mock-repository";
@@ -11,6 +13,10 @@ import { MeetingService } from "./meeting.service";
     {
       provide: MEETING_REPOSITORY,
       useClass: MockMeetingRepository,
+    },
+    {
+      provide: CURRENT_MEMBER_ADAPTER,
+      useClass: MockCurrentMemberAdapter,
     },
   ],
   exports: [MeetingService],
