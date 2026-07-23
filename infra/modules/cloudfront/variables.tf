baf5c0031e7ba82059729c83831e0444653cc7df
Developer-EJ <ejaj1217@gmail.com>
2026-07-23 15:55:42 +0900

    feat: legacy frontend redirect 추가 (#1737)

--- infra/modules/cloudfront/variables.tf
+++ infra/modules/cloudfront/variables.tf
@@ -23,3 +23,23 @@ variable "acm_certificate_arn" {
   type    = string
   default = ""
 }
+
+variable "canonical_frontend_origin" {
+  type    = string
+  default = ""
+}
+
+variable "legacy_redirect_hostnames" {
+  type    = list(string)
+  default = []
+}
+
+variable "legacy_redirect_status_code" {
+  type    = number
+  default = 302
+
+  validation {
+    condition     = contains([302, 308], var.legacy_redirect_status_code)
+    error_message = "legacy_redirect_status_code must be 302 or 308."
+  }
+}
